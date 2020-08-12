f690799f0396f677e39a8ee0f15c8429a77d1249
bkilambi <45800657+bkilambi@users.noreply.github.com>
2020-08-12 09:14:12 -0700

    with plist

---
+++ Vivid-master-3/Vivid/Vivid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vivid
+//
+//  Created by Amira Horam on 8/10/20.
+//  Copyright © 2020 Amira Horam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
